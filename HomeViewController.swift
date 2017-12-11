439d3cd054351fbe313d85b394799dd1f80c2123
Tony <sirica.tony@gmail.com>
2017-12-11 15:41:37 +0100

    Made the nav bar invisible to save space for search bar

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+        
         // Do any additional setup after loading the view.
     }
 
