43adcedf08c29f6259f4db69f5751a133b7e33e6
inaldor <inaldo.r@gmail.com>
2017-12-13 15:59:43 +0100

    Tasks finalized - Segment control and Table Views working great

--- TasksViewController.swift
+++ TasksViewController.swift
@@ -35,6 +35,22 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         PreviewDetail(title: "Large 2", preferredHeight: 0.0) // 0.0 to get the default height.
     ]
     
+    let sampleData2 =
+        
+        [
+            PreviewDetail(title: "Small 3", preferredHeight: 160.0),
+            PreviewDetail(title: "Medium 3", preferredHeight: 320.0),
+            PreviewDetail(title: "Large 3", preferredHeight: 0.0) // 0.0 to get the default height.
+    ]
+    
+    let sampleData3 =
+        
+        [
+            PreviewDetail(title: "Small 4", preferredHeight: 160.0),
+            PreviewDetail(title: "Medium 4", preferredHeight: 320.0),
+            PreviewDetail(title: "Large 4", preferredHeight: 0.0) // 0.0 to get the default height.
+    ]
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -50,19 +66,39 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        var count:Int?
+        var count = 0
         
-        if tableView == self.TableView
+        switch(Controller.selectedSegmentIndex)
         {
-            count = sampleData.count
+        case 0:
+            if tableView == self.TableView
+            {
+                count = sampleData.count
+            }
+            
+            if tableView == self.SecondTableView
+            {
+                count = sampleData1.count
+            }
+            break
+        
+        case 1:
+            if tableView == self.TableView
+            {
+                count = sampleData2.count
+            }
+            
+            if tableView == self.SecondTableView
+            {
+                count = sampleData3.count
+            }
+            break
+            
+        default:
+            break
         }
         
-        if tableView == self.SecondTableView
-        {
-            count = sampleData1.count
-        }
-        
-        return count!
+        return count
         
     }
     
@@ -71,40 +107,51 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         var cell: UITableViewCell?
         
-        if tableView == self.TableView
+        switch(Controller.selectedSegmentIndex)
         {
-            cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
-            let previewDetail = sampleData[indexPath.row]
-            cell!.textLabel!.text = previewDetail.title
+            
+        case 0:
+            if tableView == self.TableView
+            {
+                cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+                let previewDetail = sampleData[indexPath.row]
+                cell!.textLabel!.text = previewDetail.title
+            }
+            
+            if tableView == self.SecondTableView
+            {
+                cell = tableView.dequeueReusableCell(withIdentifier: "secondCell", for: indexPath)
+                let previewDetail = sampleData1[indexPath.row]
+                cell!.textLabel!.text = previewDetail.title
+            }
+            break
+            
+        case 1:
+            if tableView == self.TableView
+            {
+                cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+                let previewDetail = sampleData2[indexPath.row]
+                cell!.textLabel!.text = previewDetail.title
+            }
+            
+            if tableView == self.SecondTableView
+            {
+                cell = tableView.dequeueReusableCell(withIdentifier: "secondCell", for: indexPath)
+                let previewDetail = sampleData3[indexPath.row]
+                cell!.textLabel!.text = previewDetail.title
+            }
+            break
+        
+        default:
+            break
         }
-        
-        if tableView == self.SecondTableView
-        {
-            cell = tableView.dequeueReusableCell(withIdentifier: "secondCell", for: indexPath)
-            let previewDetail = sampleData1[indexPath.row]
-            cell!.textLabel!.text = previewDetail.title
-        }
-        
-        
         return cell!
-        
     }
     
     @IBAction func Controller(_ sender: UISegmentedControl)
     {
-        
-        if Controller.selectedSegmentIndex == 0
-        {
-            
-//            Label.text = "Hello World"
-            
-        }
-        if Controller.selectedSegmentIndex == 1
-        {
-//            Label.text = "Hello Intersect"
-            
-        }
-        
+        TableView.reloadData()
+        SecondTableView.reloadData()
     }
 
 }
