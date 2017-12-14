5a28c329c2ce319dd81dab373a91007a81f3c71f
Davide Cifariello <davide.cifariello@gmail.com>
2017-12-14 12:18:23 +0100

    Keyboard dismiss enter

--- Inaldo&Tony/SignupViewController.swift
+++ Inaldo&Tony/SignupViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, UITextFieldDelegate {
     
     //vars
     var coding = ""
@@ -43,6 +43,7 @@ class SignupViewController: UIViewController {
         //picker
         datePicker.setValue(UIColor.white, forKeyPath: "textColor")
         
+        self.nameTextField.delegate = self
         //Edit the placeholder into the main storyboard
         
     }
@@ -58,58 +59,15 @@ class SignupViewController: UIViewController {
         self.navigationController?.isNavigationBarHidden = false
     }
 
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //touch outside keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
-    */
     
-    
-    /*
-     class SecondViewController: UIViewController {
-     
-     @IBOutlet weak var textField: UITextField!
-     @IBAction func swipeDown(_ sender: Any) {
-     Write.shared.writing = textField.text!
-     self.dismiss(animated: true, completion: nil)
-     }
-     override func viewDidLoad() {
-     super.viewDidLoad()
-     
-     // Do any additional setup after loading the view.
-     }
-     
-     override func didReceiveMemoryWarning() {
-     super.didReceiveMemoryWarning()
-     // Dispose of any resources that can be recreated.
-     }
-     
-     
-     
-     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-     self.view.endEditing(true)
-     }
-     
-     
-     
-     /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-     
-     }
-     */
+    //press return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return (true)
+    }
 
 }
