19da4e23d30ba18207e61f6a1e8746aaa1b1ee69
inaldor <inaldo.r@gmail.com>
2017-12-14 23:19:52 +0100

    First changes on Profile Tab

--- ProfileViewController.swift
+++ ProfileViewController.swift
@@ -8,12 +8,56 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var skillLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
 
-    override func viewDidLoad() {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    
+    @IBOutlet weak var profileTableView: UITableView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    var skill = ["Coding", "Desing", "Economics", "Languages", "Others", "Musical Instruments"]
+    var profileDescription = ["Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is Coding is", "Desing is Design Desing is Design Desing is Design Desing is Design", "Economics is Economics is Economics is Economics", "Languages is Languages is Languages is Languages", "Others is Others is Others is Others is Others is", "MI is MI"]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width / 2
+        self.profileImageView.clipsToBounds = true
+        
+        profileTableView.dataSource = self
+        profileTableView.delegate = self
+        
+        profileTableView.estimatedRowHeight = 30
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        let count = skill.count
+        return count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as? ProfileTableViewCell
+        
+        let skillForLabel = skill[indexPath.row]
+        let descriptionForLabel = profileDescription[indexPath.row]
 
-        // Do any additional setup after loading the view.
+        cell!.skillLabel!.text = skillForLabel
+        cell!.descriptionLabel!.text = descriptionForLabel
+        
+        return cell!
     }
 
 
