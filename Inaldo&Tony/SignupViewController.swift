74cecf08af08c632fe3ab9ef04756034654cdee6
Davide Cifariello <davide.cifariello@gmail.com>
2017-12-15 11:12:53 +0100

    Sign up not working at all

--- Inaldo&Tony/SignupViewController.swift
+++ Inaldo&Tony/SignupViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class SignupViewController: UIViewController, UITextFieldDelegate {
     
+    let ourGreen = UIColor(red:0.31, green:0.82, blue:0.30, alpha:0.0)
+    
+    
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     
     //vars
@@ -19,6 +22,7 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
     var language = ""
     var science = ""
     var other = ""
+    var flag: String = ""
 
     
     //Hide keyboard when user taps anywhere
@@ -43,18 +47,20 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var datePicker: UIDatePicker!
     
     
-    @IBOutlet weak var codingButton: RoundedButton!
-    @IBOutlet weak var designButton: RoundedButton!
-    @IBOutlet weak var businessButton: RoundedButton!
-    @IBOutlet weak var languageButton: RoundedButton!
-    @IBOutlet weak var scienceButton: RoundedButton!
-    @IBOutlet weak var otherButton: RoundedButton!
+    @IBOutlet weak var codingButton: UIButton!
+    @IBOutlet weak var designButton: UIButton!
+    @IBOutlet weak var businessButton: UIButton!
+    @IBOutlet weak var languageButton: UIButton!
+    @IBOutlet weak var scienceButton: UIButton!
+    @IBOutlet weak var otherButton: UIButton!
     
     @IBAction func codingAction(_ sender: Any) {
         if !codingButton.isSelected {
             deselectAllButtons()
             codingButton.isSelected = true
             descriptionTextField.text! = coding
+            flag = "coding"
+            print("CODING BUTTON: FLAG \(flag)")
         }
         
         
@@ -66,6 +72,8 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
             deselectAllButtons()
             designButton.isSelected = true
             descriptionTextField.text! = design
+            flag = "design"
+            print("CODING DESIGN: FLAG \(flag)")
         }
     }
     
@@ -75,6 +83,7 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
             deselectAllButtons()
             businessButton.isSelected = true
             descriptionTextField.text! = business
+            flag = "business"
         }
     }
     
@@ -84,6 +93,7 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
             deselectAllButtons()
             languageButton.isSelected = true
             descriptionTextField.text! = language
+            flag = "language"
         }
     }
     
@@ -93,6 +103,7 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
             deselectAllButtons()
             scienceButton.isSelected = true
             descriptionTextField.text! = science
+            flag = "science"
         }
     }
     
@@ -101,6 +112,7 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
             deselectAllButtons()
             otherButton.isSelected = true
             descriptionTextField.text! = other
+            flag = "other"
         }
     }
     
@@ -161,8 +173,15 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-       
+        
     }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        descriptionFill()
+        print("TEXTFIELD DID END \(flag)")
+    }
+    
+
     
     @objc func keyboardWillShow(notification: Notification) {
     let userInfo:NSDictionary = notification.userInfo! as NSDictionary
@@ -187,6 +206,81 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
         languageButton.isSelected = false
         scienceButton.isSelected = false
         otherButton.isSelected = false
+        
+    }
+    
+    
+    func descriptionFill() {
+        
+        switch flag {
+            
+        case "coding":
+            coding = descriptionTextField.text!
+            if coding != "" {
+                codingButton.backgroundColor = UIColor.green
+                codingButton.titleLabel?.textColor = UIColor.black
+                print("\(flag) not empty!")
+            } else {
+                codingButton.backgroundColor = UIColor.black
+                print("\(flag) empty!")
+            }
+            
+        case "design":
+            design = descriptionTextField.text!
+            if design != "" {
+                designButton.backgroundColor = UIColor.green
+                businessButton.titleLabel?.textColor = UIColor.black
+                print("\(flag) not empty!")
+            } else {
+                designButton.backgroundColor = UIColor.black
+                print("\(flag) empty!")
+            }
+        
+        case "business":
+            business = descriptionTextField.text!
+            if business != "" {
+                businessButton.backgroundColor = UIColor.green
+                businessButton.titleLabel?.textColor = UIColor.black
+            } else {
+                businessButton.backgroundColor = UIColor.black
+            }
+        case "language":
+            language = descriptionTextField.text!
+            if language != "" {
+                languageButton.backgroundColor = UIColor.green
+                languageButton.titleLabel?.textColor = UIColor.black
+            } else {
+                languageButton.backgroundColor = UIColor.black
+            }
+            
+        case "science":
+            science = descriptionTextField.text!
+            if science != "" {
+                scienceButton.backgroundColor = UIColor.green
+                scienceButton.titleLabel?.textColor = UIColor.black
+            } else {
+                scienceButton.backgroundColor = UIColor.black
+                
+            }
+        
+        case "other":
+            other = descriptionTextField.text!
+            if other != "" {
+                otherButton.backgroundColor = UIColor.green
+                otherButton.titleLabel?.textColor = UIColor.black
+            } else {
+                otherButton.backgroundColor = UIColor.black
+            }
+            
+            
+            
+        default:
+            deselectAllButtons()
+            
+        }
+        
+        
+        
         
     }
 
